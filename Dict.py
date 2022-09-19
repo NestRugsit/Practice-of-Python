3a34ad7b45f8e153f29fd15c8a401a8e39eaf0e2
Rugsit Rungrattanachai <108135459+NestRugsit@users.noreply.github.com>
2022-09-19 17:51:45 +0700

    Add files via upload

---
+++ Dict.py
@@ -0,0 +1,12 @@
+dic = {"Robert" : 35,"Benedict" : 45,"Harry" : 28,"Timothee" : 26,"Louis" : 18, }
+name = (input( ))
+if name == "Robert" :
+  print(dic["Robert"])
+elif name == "Benedict" :
+  print(dic["Benedict"])
+elif name == "Harry" :
+  print(dic["Harry"])
+elif name == "Timothee" :
+  print(dic["Timothee"])
+elif name == "Louis" :
+  print(dic["Louis"])
---
+++ calculate.py
@@ -0,0 +1,9 @@
+num = int(input())
+i = 0
+sum = 0
+while(i < num):
+  price = int(input())
+  sum += price
+  i += 1
+
+print(sum) 
